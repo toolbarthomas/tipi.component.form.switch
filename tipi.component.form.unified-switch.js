aa2eebc9b7b612ba9605823ade75400f4b0c12bb
Thomas van der Velde <contact@toolbarthomas.net>
2017-06-13 14:14:14 +0200

    IIFE addition

--- tipi.component.form.unified-switch.js
+++ tipi.component.form.unified-switch.js
@@ -1,242 +1,215 @@
-function setUnifiedSwitch()
-{
-	var data = {
-		elements : {
-			'switch' : 'switch',
-			'control' : 'switch-control',
-			'label'	: 'label',
-			'input' : 'input:checkbox'
-		},
-		states : {
-			'ready' : '__switch--ready',
-			'focus' : '__switch--focus',
-			'active' : '__switch--active',
-			'disabled' : '__switch--disabled'
-		},
-		attributes : {
-			disabled: 'disabled',
-			checked : 'checked'
+(function (win, doc, $){
+	window.setUnifiedSwitch = function () {
+		var data = {
+			elements: {
+				'switch': 'switch',
+				'control': 'switch-control',
+				'label': 'label',
+				'input': 'input:checkbox'
+			},
+			states: {
+				'ready': '__switch--ready',
+				'focus': '__switch--focus',
+				'active': '__switch--active',
+				'disabled': '__switch--disabled'
+			},
+			attributes: {
+				disabled: 'disabled',
+				checked: 'checked'
+			}
 		}
-	}
 
-	var unified_switch = $('.' + data.elements.switch).not('.' + data.states.ready);
-	if(unified_switch.length === 0)
-	{
-		return;
-	}
-
-	unified_switch.on({
-		'tipi.unified-switch.reset' : function(event, unified_switch)
-		{
-			setUnifiedSwitchDisabledState(unified_switch, data);
-			setUnifiedSwitchCheckedState(unified_switch, data);
-		},
-		'tipi.unified-switch.toggle' : function(event, unified_switch)
-		{
-			toggleUnifiedSwitch(unified_switch, data);
-		},
-		'tipi.unified-switch.focus' : function(event, unified_switch)
-		{
-			focusUnifiedSwitch(unified_switch, data);
-		},
-		'tipi.unified-switch.blur' : function(event, unified_switch)
-		{
-			blurUnifiedSwitch(unified_switch, data);
-		},
-		'tipi.unified-switch.change' : function(event, unified_switch)
-		{
-			setUnifiedSwitchCheckedState(unified_switch, data);
-		}
-	});
-
-	unified_switch.each(function() {
-		var unified_switch = $(this);
-		var unified_switch_control = getUnifiedSwitchElement(unified_switch, 'control', data);
-		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
-
-		if(input.length === 0)
-		{
+		var unified_switch = $('.' + data.elements.switch).not('.' + data.states.ready);
+		if (unified_switch.length === 0) {
 			return;
 		}
 
 		unified_switch.on({
-			click : function(event) {
-				event.preventDefault();
-
-				//Toggle the checked state on the wrapped input
-				unified_switch.trigger('tipi.unified-switch.toggle', [$(this)]);
+			'tipi.unified-switch.reset': function (event, unified_switch) {
+				setUnifiedSwitchDisabledState(unified_switch, data);
+				setUnifiedSwitchCheckedState(unified_switch, data);
+			},
+			'tipi.unified-switch.toggle': function (event, unified_switch) {
+				toggleUnifiedSwitch(unified_switch, data);
+			},
+			'tipi.unified-switch.focus': function (event, unified_switch) {
+				focusUnifiedSwitch(unified_switch, data);
+			},
+			'tipi.unified-switch.blur': function (event, unified_switch) {
+				blurUnifiedSwitch(unified_switch, data);
+			},
+			'tipi.unified-switch.change': function (event, unified_switch) {
+				setUnifiedSwitchCheckedState(unified_switch, data);
 			}
 		});
 
-		input.on({
-			click : function(event)
-			{
-				//Cancels out the click event that is being triggerd by clicking on the corresponding label or controlling the input with the keyboard.
-				event.stopImmediatePropagation();
-			},
-			focus : function(event) {
-				event.preventDefault();
+		unified_switch.each(function () {
+			var unified_switch = $(this);
+			var unified_switch_control = getUnifiedSwitchElement(unified_switch, 'control', data);
+			var input = getUnifiedSwitchElement(unified_switch, 'input', data);
 
-				var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
-				if(unified_switch.length === 0) {
-					return;
-				}
-
-				//Set the focus state while focusing the input
-				unified_switch.trigger('tipi.unified-switch.focus', [unified_switch]);
-			},
-			blur : function(event) {
-				event.preventDefault();
-
-				var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
-				if(unified_switch.length === 0) {
-					return;
-				}
-
-				//Remove the focus state while focusing the input
-				unified_switch.trigger('tipi.unified-switch.blur', [unified_switch]);
-			},
-			change : function(event)  {
-				var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
-				if(unified_switch.length === 0) {
-					return;
-				}
-
-				//Toggle the active state on the root container when the input has changed
-				unified_switch.trigger('tipi.unified-switch.change', [unified_switch]);
+			if (input.length === 0) {
+				return;
 			}
+
+			unified_switch.on({
+				click: function (event) {
+					event.preventDefault();
+
+					//Toggle the checked state on the wrapped input
+					unified_switch.trigger('tipi.unified-switch.toggle', [$(this)]);
+				}
+			});
+
+			input.on({
+				click: function (event) {
+					//Cancels out the click event that is being triggerd by clicking on the corresponding label or controlling the input with the keyboard.
+					event.stopImmediatePropagation();
+				},
+				focus: function (event) {
+					event.preventDefault();
+
+					var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
+					if (unified_switch.length === 0) {
+						return;
+					}
+
+					//Set the focus state while focusing the input
+					unified_switch.trigger('tipi.unified-switch.focus', [unified_switch]);
+				},
+				blur: function (event) {
+					event.preventDefault();
+
+					var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
+					if (unified_switch.length === 0) {
+						return;
+					}
+
+					//Remove the focus state while focusing the input
+					unified_switch.trigger('tipi.unified-switch.blur', [unified_switch]);
+				},
+				change: function (event) {
+					var unified_switch = getUnifiedSwitchElement($(this), 'switch', data);
+					if (unified_switch.length === 0) {
+						return;
+					}
+
+					//Toggle the active state on the root container when the input has changed
+					unified_switch.trigger('tipi.unified-switch.change', [unified_switch]);
+				}
+			});
+
+			//Set the corresponding states based on the given attributes on the input
+			unified_switch.trigger('tipi.unified-switch.reset', [unified_switch]);
+
+			//Our switch is ready!
+			unified_switch.addClass(data.states.ready);
 		});
-
-		//Set the corresponding states based on the given attributes on the input
-		unified_switch.trigger('tipi.unified-switch.reset', [unified_switch]);
-
-		//Our switch is ready!
-		unified_switch.addClass(data.states.ready);
-	});
-}
-
-function toggleUnifiedSwitch(unified_switch, data)
-{
-	var input = getUnifiedSwitchElement(unified_switch, 'input', data);
-
-	if(input.length === 0)
-	{
-		return;
 	}
 
-	if(input.prop(data.attributes.disabled))
-	{
-		return;
+	function toggleUnifiedSwitch(unified_switch, data) {
+		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
+
+		if (input.length === 0) {
+			return;
+		}
+
+		if (input.prop(data.attributes.disabled)) {
+			return;
+		}
+
+		var checked = true;
+		if (input.prop(data.attributes.checked)) {
+			checked = false;
+		}
+
+		input.prop(data.attributes.checked, checked);
+
+		input.trigger('change');
 	}
 
-	var checked = true;
-	if(input.prop(data.attributes.checked))
-	{
-		checked = false;
+	function setUnifiedSwitchCheckedState(unified_switch, data) {
+		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
+
+		if (input.length === 0) {
+			return;
+		}
+
+		if (input.prop(data.attributes.checked)) {
+			unified_switch.addClass(data.states.active);
+		}
+		else {
+			unified_switch.removeClass(data.states.active);
+		}
 	}
 
-	input.prop(data.attributes.checked, checked);
+	function focusUnifiedSwitch(unified_switch, data) {
+		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
 
-	input.trigger('change');
-}
+		if (input.length === 0) {
+			return;
+		}
 
-function setUnifiedSwitchCheckedState(unified_switch, data)
-{
-	var input = getUnifiedSwitchElement(unified_switch, 'input', data);
-
-	if(input.length === 0)
-	{
-		return;
+		unified_switch.addClass(data.states.focus);
 	}
 
-	if(input.prop(data.attributes.checked))
-	{
-		unified_switch.addClass(data.states.active);
-	}
-	else
-	{
-		unified_switch.removeClass(data.states.active);
-	}
-}
+	function blurUnifiedSwitch(unified_switch, data) {
+		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
 
-function focusUnifiedSwitch(unified_switch, data)
-{
-	var input = getUnifiedSwitchElement(unified_switch, 'input', data);
+		if (input.length === 0) {
+			return;
+		}
 
-	if(input.length === 0)
-	{
-		return;
+		unified_switch.removeClass(data.states.focus);
 	}
 
-	unified_switch.addClass(data.states.focus);
-}
+	function setUnifiedSwitchDisabledState(unified_switch, data) {
+		var input = getUnifiedSwitchElement(unified_switch, 'input', data);
 
-function blurUnifiedSwitch(unified_switch, data)
-{
-	var input = getUnifiedSwitchElement(unified_switch, 'input', data);
+		if (input.length === 0) {
+			return;
+		}
 
-	if(input.length === 0)
-	{
-		return;
+		if (input.prop(data.attributes.disabled)) {
+			unified_switch.addClass(data.states.disabled);
+		}
+		else {
+			unified_switch.removeClass(data.states.disabled);
+		}
 	}
 
-	unified_switch.removeClass(data.states.focus);
-}
+	function getUnifiedSwitchElement(origin, type, data) {
+		if (typeof origin == 'undefined') {
+			return;
+		}
 
-function setUnifiedSwitchDisabledState(unified_switch, data)
-{
-	var input = getUnifiedSwitchElement(unified_switch, 'input', data);
+		if (origin.length == 0) {
+			return;
+		}
 
-	if(input.length === 0)
-	{
-		return;
+		if (typeof data.elements == 'undefined') {
+			return;
+		}
+
+		var element = {};
+
+		switch (type) {
+			case 'switch':
+				element = origin.parents('.' + data.elements.switch).first();
+				break;
+			case 'control':
+				element = origin.find('.' + data.elements.control).first();
+				break;
+			case 'label':
+				element = origin.find(data.elements.label);
+				break;
+			case 'input':
+				element = origin.find(data.elements.input).first();
+				break;
+			default:
+				element.length = 0;
+		}
+
+		return element;
 	}
-
-	if(input.prop(data.attributes.disabled))
-	{
-		unified_switch.addClass(data.states.disabled);
-	}
-	else
-	{
-		unified_switch.removeClass(data.states.disabled);
-	}
-}
-
-function getUnifiedSwitchElement(origin, type, data)
-{
-	if(typeof origin == 'undefined')
-	{
-		return;
-	}
-
-	if(origin.length == 0)
-	{
-		return;
-	}
-
-	if(typeof data.elements == 'undefined')
-	{
-		return;
-	}
-
-	var element = {};
-
-	switch(type) {
-		case 'switch':
-			element = origin.parents('.' + data.elements.switch).first();
-		break;
-		case 'control':
-			element = origin.find('.' + data.elements.control).first();
-		break;
-		case 'label':
-			element = origin.find(data.elements.label);
-		break;
-		case 'input':
-			element = origin.find(data.elements.input).first();
-		break;
-		default:
-			element.length = 0;
-	}
-
-	return element;
-}
+})(window.jQuery(window), window.jQuery(document), window.jQuery);
